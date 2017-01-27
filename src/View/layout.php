1deb8724c7c2912b95bce548a11cf51feda45719
Florian <florian.bouchet.contact@gmail.com>
2017-01-27 14:21:44 +0100

    Modify path

--- src/View/layout.php
+++ src/View/layout.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="../../public/style/style.css">
+    <link rel="stylesheet" type="text/css" href="<?= PATH ?>/public/style/style.css">
 
     <title>Super Hero Land</title>
 </head>
