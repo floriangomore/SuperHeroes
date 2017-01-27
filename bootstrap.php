71c6fa75477757c730a0b63dc43ee249488b0add
Florian <florian.bouchet.contact@gmail.com>
2017-01-27 14:12:12 +0100

    Modify path

---
+++ bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+// Doctrine configuration
+// bootstrap.php
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+$isDevMode = true;
+$config = Setup::createAnnotationMetadataConfiguration(array("src/Entity"), $isDevMode);
+
+// database configuration parameters
+$conn = [
+    'driver' => 'pdo_sqlite',
+    'path' => __DIR__ . '/db.sqlite',
+];
+
+$em = EntityManager::create($conn, $config);
--- index.php
+++ index.php
@@ -3,16 +3,12 @@
 use src\Dispatcher;
 
 require_once "vendor/autoload.php";
+require_once "bootstrap.php";
 
 define('_PUBLIC_PATH_', __DIR__ .'\\public\\');
-if (isset($_SERVER['PATH_INFO'])&&$_SERVER['PATH_INFO']!=null){
-    $url = $_SERVER['PATH_INFO'];
-}else{
-    $url = null;
-}
 
 $path = explode(DIRECTORY_SEPARATOR, __DIR__);
 define('PATH', '/' . $path[sizeof($path)-1]);
 
-$dispatch = new Dispatcher($url,$method);
+$dispatch = new Dispatcher($em);
 echo $dispatch->dispatch();
